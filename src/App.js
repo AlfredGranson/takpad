cdf3d683dc854e9f831210e2b1c16f73eb2671d2
Alfred Granson <alfredg@gmail.com>
2024-03-11 12:21:50 -0400

    fix: Get simple pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { ApiClient, ApiProvider } from 'jsonapi-react'
+import schema from './schema'
+import { useQuery } from 'jsonapi-react'
 import './App.css';
+
+const client = new ApiClient({
+  url: 'http://localhost:3000/api/v1',
+  schema,
+})
+
+function Pages() {
+  const { data, meta, error, isLoading, isFetching } = useQuery('pages')
+
+  if(data){
+    return (
+        data.map(page => <li>{page.title}</li>)
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          We here
+          We here..
         </p>
+        <ApiProvider client={client}>
+          <Pages />
+        </ApiProvider>
       </header>
     </div>
   );
---
+++ src/schema.js
@@ -0,0 +1,5 @@
+const schema = {
+  pages: {
+    type: 'pages',
+  },
+}
